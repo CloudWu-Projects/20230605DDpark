94e7f4aad73bee64431c304d08323cf4bea79680
Dai li <li.dai@riedel.net>
2025-03-29 10:39:34 +0800

    build: specify package path in build commands
    The build commands now explicitly specify the package path (../cmd/jilaidian/) to ensure the correct package is built. This prevents potential issues when running the script from different directories.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 rm -rf bin
 # Windows 64 位
-GOOS=windows GOARCH=amd64 go build -o bin/jilaidian_go.exe
+GOOS=windows GOARCH=amd64 go build -o bin/jilaidian_go.exe ../cmd/jilaidian/
 
 
 # Linux 64 位
-GOOS=linux GOARCH=amd64 go build -o bin/jilaidian_go
+GOOS=linux GOARCH=amd64 go build -o bin/jilaidian_go ../cmd/jilaidian/
