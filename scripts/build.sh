51792d70d7d19bb09dbef49e2f93abb6bac9e0bf
Dai li <li.dai@riedel.net>
2025-06-06 12:45:11 +0200

    build: update build script for yianqi_go executable
    - Remove main.go file from yianqi_go module
    - Update build.sh script to use mainExe module for yianqi_go executable
    - Rename jilaidian_go to yianqi_go in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 rm -rf bin
 # Windows 64 位
-GOOS=windows GOARCH=amd64 go build -o bin/jilaidian_go.exe ../cmd/jilaidian/
+GOOS=windows GOARCH=amd64 go build -o bin/yianqi_go.exe ../cmd/mainExe/
 
 
 # Linux 64 位
-GOOS=linux GOARCH=amd64 go build -o bin/jilaidian_go ../cmd/jilaidian/
+GOOS=linux GOARCH=amd64 go build -o bin/yianqi_go ../cmd/mainExe/
 GOOS=linux GOARCH=amd64 go build -o bin/downloader ../cmd/download7niu/
