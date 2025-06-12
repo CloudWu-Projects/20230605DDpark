26fdb7b7c81393d8b12c66549d12bed3d52f85b1
Dai li <li.dai@riedel.net>
2025-06-12 04:29:17 +0200

    refactor(build): make build script more universal and reliable
    - Use variable for app name to make it configurable
    - Add error handling for build process to ensure reliability
    - Make output file names consistent across different OS
    - Add option to upload to Qiniu storage

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
+
+appName="yilingshequ_go"
+
+echo $appName
 rm -rf bin
 # Windows 64 位
-GOOS=windows GOARCH=amd64 go build -o bin/yilingshequ_go.exe ../cmd/mainExe/
+if ! GOOS=windows GOARCH=amd64 go build -o bin/$appName.exe ../cmd/mainExe/;then
+    echo "编译失败"
+    exit 1
+fi
 
 
 # Linux 64 位
-GOOS=linux GOARCH=amd64 go build -o bin/yilingshequ_go ../cmd/mainExe/
-GOOS=linux GOARCH=amd64 go build -o bin/downloader ../cmd/download7niu/
+if ! GOOS=linux GOARCH=amd64 go build -o bin/$appName ../cmd/mainExe/; then
+    echo "编译失败"
+    exit 1
+fi
+
+
+if  [ "$1" = "up" ]; then
+    echo "上传到七牛"
+    python ./7niu/upload_7niu.py ./bin/$appName --filename=$appName
+else
+    echo "不进行上传"
+fi
