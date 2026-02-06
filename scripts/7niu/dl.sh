c412c7cc89a7c2fc1c08e152f5010f0e86a105b5
Dai li <li.dai@riedel.net>
2026-02-06 09:29:31 +0800

    ``` fix(scripts): correct path syntax in download script
    - Fixed incorrect path reference from '.$serverName/$serverName -install'
    to './$serverName/$serverName -install' in dl.sh script
    - This ensures proper execution of the server installation command
    ```

--- scripts/7niu/dl.sh
+++ scripts/7niu/dl.sh
@@ -6,7 +6,7 @@ python3 down7niu.py $serverName/$serverName.zip
 mv $serverName/$serverName.zip $serverName/$serverName -f
 
 chmod +x ./$serverName/$serverName
-.$serverName/$serverName -install
+./$serverName/$serverName -install
 
 md5sum ./$serverName/$serverName
 
